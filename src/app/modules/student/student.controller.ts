93330939945e47d2713fdc3d3c812259c147b533
MD Nayem Al Kashem <nayemshaon34@gmail.com>
2023-12-13 02:09:52 +0600

    sending data to mongoDB

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+const createStudent = async (req:Request,res:Response)=>{
+
+//param diye parameter, query diye query parameter and body diye pura boro data anbo
+try {
+    // const student = req.body.student;
+    const {student:studentData/*name elias*/} = req.body;
+
+//will call service function to send this data
+
+const result = await StudentServices.createStudentIntoDB(studentData)
+ //send response
+
+ res.status(200).json({
+    success:true,
+    message:"student is created succesfully",
+    data:result
+ });
+} catch (error) {
+    console.log(error);
+}
+};
+
+export const StudentControllers = {
+    createStudent,
+}
--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -54,4 +54,4 @@ const studentSchema = new Schema<Student>({
 
   // 3. Create a Model.
 
-  const Student = model<Student>('student',studentSchema);
+  export const StudentModel = model<Student>('Student',studentSchema);
