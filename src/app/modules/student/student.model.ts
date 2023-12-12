61fb293af6c2656b56e5ff28ae758063f85afeb6
MD Nayem Al Kashem <nayemshaon34@gmail.com>
2023-12-13 00:00:10 +0600

    schema model done

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,24 +1,42 @@
-import { Schema } from "mongoose";
-import { Student } from "./student.interface";
+import { Schema, model } from "mongoose";
+import { Guardian, LocalGuardian, Student, UserName } from "./student.interface";
 
 
 // 2. Create a Schema corresponding to the document interface.
+const userNameSchema = new Schema<UserName>({
+    firstName:{
+        type:String,
+        required:true,
+    },
+    middleName:{
+        type:String,
+        required:true,
+    },
+    lastName:{
+        type:String,
+        required:true,
+    },
+});
+
+const guardianSchema = new Schema<Guardian>({
+    fatherName:{type:String,required:true},
+    fatherContactNo:{type:String,required:true},
+    fatherOccupation:{type:String,required:true},
+    motherName:{type:String,required:true},
+    motherContactNo:{type:String,required:true},
+    motherOccupation:{type:String,required:true},
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+    name:{type:String,required:true},
+    occupation:{type:String,required:true},
+    contactNo:{type:String,required:true},
+    address:{type:String,required:true},
+});
+
 const studentSchema = new Schema<Student>({
     id:{type:String},
-    name:{
-        firstName:{
-            type:String,
-            required:true,
-        },
-        middleName:{
-            type:String,
-            required:true,
-        },
-        lastName:{
-            type:String,
-            required:true,
-        },
-    },
+    name:userNameSchema,
     gender: ["male","female"], // predifned value thakle enum use korbo
     dateOfBirth:{type:String},
     email:{type:String, required:true},
@@ -27,23 +45,13 @@ const studentSchema = new Schema<Student>({
     blood_group:['A','B','AB','O','A+','A-','B+','B-','AB+','AB-','O+','O-'],
     presentAddress:{type:String,required:true},
     permanantAddress:{type:String,required:true},
-    guardian:{
-        fatherName:{type:String,required:true},
-        fatherContactNo:{type:String,required:true},
-        fatherOccupation:{type:String,required:true},
-        motherName:{type:String,required:true},
-        motherContactNo:{type:String,required:true},
-        motherOccupation:{type:String,required:true},
-    },
-    localGuardian:{
-        name:{type:String,required:true},
-        occupation:{type:String,required:true},
-        contactNo:{type:String,required:true},
-        address:{type:String,required:true},
-    },
-    profileImg:{type:String}
+    guardian:guardianSchema,
+    localGuardian:localGuardianSchema,
+    profileImg:{type:String},
     isActive:['active','inActive'],
 })
 
 
   // 3. Create a Model.
+
+  const Student = model<Student>('student',studentSchema);
