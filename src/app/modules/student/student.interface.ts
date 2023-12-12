306ab73396d8892a17433ed7257ebb16003c475d
MD Nayem Al Kashem <nayemshaon34@gmail.com>
2023-12-12 22:16:50 +0600

    student interface done

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,41 @@
+//created interface
+export type Guardian = {
+    fatherName:string;
+    fatherOccupation:string;
+    fatherContactNo:string;
+    motherName:string;
+    motherOccupation:string;
+    motherContactNo:string;
+};
+
+export type UserName = {
+    firstName:string;
+    middleName:string;
+    lastName:string;
+};
+
+export type LocalGuardian = {
+    name:string;
+    occupation:string;
+    contactNo:string;
+    address:string;
+};
+
+
+export type Student = {
+    id:string;
+    name:UserName;
+    gender:'male' | "female"; //union type literal
+    email:string;
+    dateOfBirth:string;
+    avatar?:string;
+    contactNo:string;
+    emergencyContactNo:string;
+    blood_group?:'A' | 'B' | 'AB' | 'O' | 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+    presentAddress:string;
+    permanantAddress:string;
+    guardian:Guardian;
+    localGuardian:LocalGuardian;
+    profileImg?:string;
+    isActive:"active"|"inActive";
+};
