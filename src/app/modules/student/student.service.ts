4424b65bd76dd8ad9b4b99fafaff5e577168d4f0
MD Nayem Al Kashem <nayemshaon34@gmail.com>
2023-12-15 20:08:11 +0600

    get all students from service done

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -4,8 +4,14 @@ import { StudentModel } from "./student.model";
 const createStudentIntoDB = async (student:Student)=>{
     const result = await StudentModel.create(student);
     return result;
+};
+
+const getAllStudentsFromDB = async ()=>{
+    const result = await StudentModel.find();
+    return result;
 }
 
 export const StudentServices = {
     createStudentIntoDB,
+    getAllStudentsFromDB
 }
