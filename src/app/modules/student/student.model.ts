3ad601ab6d4dab086f3885bf573c8177d869fbbf
MD Nayem Al Kashem <nayemshaon34@gmail.com>
2023-12-16 21:33:50 +0600

    custom validation mssg updated

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -35,20 +35,48 @@ const localGuardianSchema = new Schema<LocalGuardian>({
 });
 
 const studentSchema = new Schema<Student>({
-    id:{type:String},
-    name:userNameSchema,
-    gender: ["male","female"], // predifned value thakle enum use korbo
+    id:{type:String, required:true, unique:true},
+    name:{
+        type:userNameSchema,
+        required:[true,'name must enter kortei hobe'],
+    },
+    gender: {
+        type: String,
+        enum : {
+            values:["male","female"],
+            message:"the gender can only be one of the follwing: 'male','female'"
+        },
+        required: true,
+    }, // predifned value thakle enum use korbo
     dateOfBirth:{type:String},
     email:{type:String, required:true},
     contactNo:{type:String,required:true},
     emergencyContactNo:{type:String,required:true},
-    blood_group:['A','B','AB','O','A+','A-','B+','B-','AB+','AB-','O+','O-'],
+    blood_group:{
+        type:String,
+        enum:{
+            values:['A','B','AB','O','A+','A-','B+','B-','AB+','AB-','O+','O-'],
+            message:'{VALUE} is not valid'
+        },
+        required:true,
+    },
     presentAddress:{type:String,required:true},
     permanantAddress:{type:String,required:true},
-    guardian:guardianSchema,
-    localGuardian:localGuardianSchema,
+    guardian:{
+        type:guardianSchema,
+        required:true,
+    },
+    localGuardian:{
+        type:localGuardianSchema,
+        required:true,
+    },
     profileImg:{type:String},
-    isActive:['active','inActive'],
+    isActive:{
+        type:String,
+        enum:['active','inActive'],
+        required:true,
+        default:'active',
+    },
 })
 
 
